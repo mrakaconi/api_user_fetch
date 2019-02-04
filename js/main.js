9108bfe74a0b8a27c675b4ec135ee2eaedfdaabb
Marko Jordanovic <mrakaconi@gmail.com>
2019-02-04 17:10:12 +0100

    Prva iteracija upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// $.ajax('https://randomuser.me/api/?results=20')
+//     .done(function (korisnici) {
+//         console.log(korisnici);
+
+// for (i = 0; i <= korisnici.results.length; i++) {
+
+//     $("#okvir").append("<div class=blah></div>");
+//     $(".blah").append("<h1>" + korisnici.results[i].name.first + " " + korisnici.results[i].name.last + "</h1>"),
+//     $(".blah").append("<p>" + korisnici.results[i].email + "</p>"),
+//     $(".blah").append("<p>" + korisnici.results[i].phone + "</p>")
+//     $(".blah").append(`<img src="${korisnici.results[i].picture.medium}">`)
+
+// };
+// $.ajax('https://randomuser.me/api/?results=20')
+//     .done(function (korisnici) {
+//         console.log(korisnici);
+
+
+// for (i = 0; i < korisnici.results.length; i++) {
+//     $mika = $("<div class=blah></div>");
+//     $mika.append("<h1>" + korisnici.results[i].name.title + " " + korisnici.results[i].name.first + " " +        korisnici.results[i].name.last + "</h1>"),
+//     $mika.append("<p>" + korisnici.results[i].email + "</p>"),
+//     $mika.append("<p>" + korisnici.results[i].phone + "</p>")
+//     $mika.append(`<img src="${korisnici.results[i].picture.medium}">`)
+
+//     $("#okvir").append($mika);
+// };
+// })
+
+
+
+
+
+
